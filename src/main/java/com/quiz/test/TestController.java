9485082fbabaf0bacde29527b2dc46baa188b1bc
sujung00 <rlatnwjdirin@naver.com>
2023-03-20 21:42:28 +0900

    MyBatis 연동

--- src/main/java/com/quiz/test/TestController.java
+++ src/main/java/com/quiz/test/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @Controller
 public class TestController {
 
