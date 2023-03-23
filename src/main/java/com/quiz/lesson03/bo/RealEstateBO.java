85fdf54103d1264ade54c1baf635d1be157327ad
sujung00 <rlatnwjdirin@naver.com>
2023-03-23 21:44:55 +0900

    JSP와 Form 태그

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -33,4 +33,12 @@ public class RealEstateBO {
 	public int addRealEstateAsField(int realtorId, String address, int area, String type, int price, Integer rentPrice) {
 		return realEstateMapper.insertRealEstateAsField(realtorId, address, area, type, price, rentPrice);
 	}
+	
+	public int updateRealEstateById(int id, String type, int price) {
+		return realEstateMapper.updateRealEstateById(id, type, price);
+	}
+	
+	public int deleteRealEstateById(int id) {
+		return realEstateMapper.deleteRealEstateById(id);
+	}
 }
